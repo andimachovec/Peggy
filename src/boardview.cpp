21077d8d2a7126d4d7487534b3491f7664893654
Andi Machovec <andi.machovec@gmail.com>
2021-11-23 22:42:23 +0100

    started implementing the basic peg drawing and positioning functions

--- src/boardview.cpp
+++ src/boardview.cpp
@@ -1,15 +1,75 @@
 #include "boardview.h"
 
 
+BoardRow::BoardRow(BView *target_view)
+{
+
+	//initialize pegs
+	rgb_color init_color;
+	init_color.set_to(54, 34, 4);
+
+	for(int i = 0; i < 4 ; ++i)
+	{
+		fColorPegs[i] = new Peg(target_view, BPoint(), 20, init_color);
+		fResultPegs[i] = new Peg(target_view, BPoint(), 5, init_color);
+	}
+
+}
+
+
+BoardRow::~BoardRow()
+{
+
+	for(int i = 0; i < 4 ; ++i)
+	{
+		delete fColorPegs[i];
+		delete fResultPegs[i];
+	}
+
+}
+
+
+Peg*
+BoardRow::GetColorPeg(int peg_nr)
+{
+
+	return fColorPegs[peg_nr];
+
+}
+
+
+Peg*
+BoardRow::GetResultPeg(int peg_nr)
+{
+
+	return fResultPegs[peg_nr];
+
+}
+
+
 BoardView::BoardView()
 	:
 	BView("boardview", B_SUPPORTS_LAYOUT|B_WILL_DRAW)
 {
 
+	//initialize rows
+	fColorPegRadius = 20;
 
+	for(int i = 0; i < 9 ; ++i)
+	{
+		fRows[i] = new BoardRow(this);
+	}
 
 }
 
+
+BoardView::~BoardView()
+{
+	for(int i = 0; i < 9 ; ++i)
+	{
+		delete fRows[i];
+	}
+}
 
 void
 BoardView::Draw(BRect updateRect)
@@ -17,5 +77,44 @@ BoardView::Draw(BRect updateRect)
 
 	SetLowColor(101,67,33);
 	FillRect(updateRect, B_SOLID_LOW);
+
+	for(int row = 0; row < 9; ++row)
+	{
+		for(int peg_nr = 0; peg_nr < 4; ++peg_nr)
+		{
+			fRows[row]->GetColorPeg(peg_nr)->Draw();
+			//fRows[row]->GetResultPeg(peg_nr)->Draw();
+		}
+	}
+
+}
+
+
+void
+BoardView::LayoutChanged()
+{
+
+	float delta_x = (Bounds().Width() - 8*fColorPegRadius) / 5;
+	float delta_y = (Bounds().Height() - 18*fColorPegRadius) / 10;
+
+	BPoint peg_center;
+	peg_center.x = delta_x+fColorPegRadius;
+	peg_center.y = Bounds().Height()-delta_y-fColorPegRadius;
+
+
+	BPoint peg_test = peg_center;
+	peg_test.y = delta_y+2*fColorPegRadius;
+	fRows[8]->GetColorPeg(0)->SetCenter(peg_test);
+
+	for(int row = 0; row < 9 ; ++row)
+	{
+		for(int peg_nr = 0; peg_nr < 4; ++peg_nr)
+		{
+			fRows[row]->GetColorPeg(peg_nr)->SetCenter(peg_center);
+			peg_center.x+=delta_x+2*fColorPegRadius;
+		}
+
+		peg_center.y-=delta_y-2*fColorPegRadius;
+	}
 
 }
--- src/boardview.h
+++ src/boardview.h
@@ -3,13 +3,33 @@
 
 
 #include <View.h>
+#include <array>
+#include "peg.h"
+
+
+class BoardRow {
+public:
+	BoardRow(BView *target_view);
+	~BoardRow();
+	Peg* GetColorPeg(int peg_nr);
+	Peg* GetResultPeg(int peg_nr);
+
+private:
+	std::array<Peg*, 4> fColorPegs;
+	std::array<Peg*, 4> fResultPegs;
+};
 
 
 class BoardView : public BView {
 public:
 	BoardView();
+	~BoardView();
 	void Draw(BRect updateRect);
+	void LayoutChanged();
+
 private:
+	std::array<BoardRow*, 9> fRows;
+	float fColorPegRadius;
 
 };
 
